110c609dfaf50fd0aff2b01e656ba7f5a69b1655
Yu Li <yli0607x@gmail.com>
2018-09-20 01:47:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,3 +35,8 @@ function destructivelyRemoveElementFromEndOfArray(array) {
    array.pop()
    return array
 }
+
+function removeElementFromEndOfArray(array) {
+  array.slice(0, array.length - 1)
+  return array
+}
